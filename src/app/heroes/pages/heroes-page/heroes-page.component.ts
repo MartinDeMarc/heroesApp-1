fb67e38092b0455b6bc560018860729fbb135e55
Martin Enriquez <martin.enriquez.seb@gmail.com>
2023-05-04 12:45:32 -0300

    style cursor pointer and heroes page

--- src/app/heroes/pages/heroes-page/heroes-page.component.ts
+++ src/app/heroes/pages/heroes-page/heroes-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import {  switchMap } from 'rxjs';
 import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
@@ -16,8 +16,9 @@ export class HeroesPageComponent implements OnInit{
   constructor(private heroesService :HeroesService, private activatedRoute: ActivatedRoute, private router: Router){}
 
   ngOnInit(): void {
+
     this.activatedRoute.params
-    .pipe(  switchMap(({id}) => this.heroesService.getHeroById(id)),).subscribe( hero => { if (!hero) return this.router.navigate (['/heroes/list']);
+    .pipe(   switchMap(({id}) => this.heroesService.getHeroById(id)),).subscribe( hero => { if (!hero) return this.router.navigate (['/heroes/list']);
   this.hero = hero;
   console.log({hero});
 
@@ -25,6 +26,10 @@ export class HeroesPageComponent implements OnInit{
   return;
 
 })
+  }
+  goBack():void{
+
+    this.router.navigateByUrl('/heroes/list')
   }
 
 }
