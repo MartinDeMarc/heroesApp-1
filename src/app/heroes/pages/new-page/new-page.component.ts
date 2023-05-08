8e3422e84deba1c83b70a1754a4425ba18626850
Martin Enriquez <martin.enriquez.seb@gmail.com>
2023-05-08 15:02:09 -0300

    create form group & form control

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-page.component.css']
 })
 export class NewPageComponent {
+
+  public heroFrom = new FormGroup({
+    id: new FormControl<string>(''),
+  superhero: new FormControl<string>('', {nonNullable: true}),
+  publisher: new FormControl<Publisher>(Publisher.DCComics),
+  alter_ego: new FormControl(''),
+  first_appearance: new FormControl(''),
+  characters: new FormControl(''),
+  alt_img: new FormControl(''),
+
+  })  // adentro definimos todas las propiedas que queremos que nuestro formulario maneje
+
+
+  public publishers = [
+   {  id: "Dc Comics", desc: "DC - Comics"},
+    {id: "Marvel Comics", desc: "Marvel - Comics"},
+  ]
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
-import {  Observable, catchError, of } from "rxjs";
+import {  Observable, catchError, map, of } from "rxjs";
 import { environments } from 'src/environments/environment';
 
 @Injectable({providedIn: 'root'})
@@ -24,5 +24,24 @@ private baseUrl: string = environments.baseUrl;
 
   getSuggestions(query:string):Observable<Hero[]>{
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`)
+  }
+
+
+  addHero(hero:Hero):Observable<Hero>  {
+return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }   //addHero regresa un observable y un Hero al final con el id o con la data del objeto
+
+  updateHero(hero:Hero):Observable<Hero>  {
+    if (!hero.id) throw Error ("Hero id is required")
+    return this.httpClient.post<Hero>(`${this.baseUrl}/{hero.id}`, hero);
+      }
+
+  deleteHeroById(id:string ):Observable<boolean>{
+    return this.httpClient.delete(`${this.baseUrl}/{hero.id}`)
+    .pipe(
+      catchError(err => of(false) ),
+      map( resp => true)
+    )
+
   }
 }
