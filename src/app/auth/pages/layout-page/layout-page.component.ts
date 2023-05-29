00824e4012546bab61ae2b94f6bbc299ff23a08e
Martin Enriquez <martin.enriquez.seb@gmail.com>
2023-05-29 15:35:49 -0300

    bug solucionado

--- src/app/auth/pages/layout-page/layout-page.component.ts
+++ src/app/auth/pages/layout-page/layout-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styleUrls: ['./layout-page.component.css']
+  styleUrls: [],
 })
 export class LayoutPageComponent {
 
